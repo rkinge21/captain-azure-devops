2596c3f1017e01f9f09221379f5765ee05efabf2
Rahul Kinge <rkinge81@gmail.com>
2024-09-16 15:01:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Captain Rahul Kinge.</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Azure DevOps CI-CD via this React Application.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
